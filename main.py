b641c48f38d5f3d0fe9f93561c556ac5e501728f
CaptMoroni <dustiumus@gmail.com>
2024-03-07 13:31:54 -0700

    final commit

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from typing import Final
-import os 
+import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 
 # STEP 0: Load Our Token From A Safe Place
-
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 print(TOKEN)
